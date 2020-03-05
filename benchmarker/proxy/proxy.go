9af59e6a810104b3e1689a3e5db606ebe879d40c
M. Fatih MAR <mfatihmar@gmail.com>
2020-03-05 01:01:02 +0000

    Add Proxy

---
+++ benchmarker/proxy/proxy.go
@@ -0,0 +1,27 @@
+package proxy
+
+import "net"
+
+type Proxy struct {
+	LastError error
+
+	FakeRecvCh chan []byte
+	FakeSendCh chan []byte
+
+	RealRecvCh chan []byte
+	RealSendCh chan []byte
+
+	fakeRecvBuf []byte
+	realRecvBuf []byte
+
+	conn *net.UDPConn
+}
+
+func NewProxy(fakePort, realPort uint16, bufferSize uint16) (*Proxy, error) {
+	// todo: check `fakePort`, `realPort` and `bufferSize` arguments
+
+	return nil, nil
+}
+
+func (proxy *Proxy) Close() {
+}
