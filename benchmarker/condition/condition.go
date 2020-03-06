4d0ebb3334e504ec9a219bcdf848cbc63818c08f
M. Fatih MAR <mfatihmar@gmail.com>
2020-03-06 16:57:47 +0000

    Add Basics for Condition

--- benchmarker/condition/condition.go
+++ benchmarker/condition/condition.go
@@ -1,4 +1,29 @@
 package condition
 
+import (
+	"errors"
+	"sync"
+)
+
 type Condition struct {
+	Err error
+
+	closeOnce sync.Once
+	closed    bool
+}
+
+type Config struct {
+}
+
+func NewCondition(cfg *Config) (*Condition, error) {
+	// todo
+	return nil, errors.New("not implemented")
+}
+
+func (c *Condition) Close() {
+	c.closeOnce.Do(func() {
+		c.closed = true
+
+		// todo
+	})
 }
