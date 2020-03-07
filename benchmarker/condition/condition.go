1b5243201efbf851ba72dfbec8c0e44166b09a2e
M. Fatih MAR <mfatihmar@gmail.com>
2020-03-07 23:09:40 +0000

    add condition functions

--- benchmarker/condition/condition.go
+++ benchmarker/condition/condition.go
@@ -13,19 +13,34 @@ type Condition struct {
 }
 
 type Config struct {
-	LatencyMsMax int   // todo: explain
-	LatencyMsMin int   // todo: explain
-	LossPercMax  int   // todo: explain
-	LossPercMin  int   // todo: explain
-	BandwidthMax int   // todo: explain
-	BandwidthMin int   // todo: explain
-	MTUs         []int // todo: explain
-	UpdateRateMs int   // todo: explain
+	LatencyMsMax  int   // todo: explain
+	LatencyMsMin  int   // todo: explain
+	LossPer1kMax  int   // todo: explain
+	LossPer1kMin  int   // todo: explain
+	BandwidthMax  int   // todo: explain
+	BandwidthMin  int   // todo: explain
+	MTUs          []int // todo: explain
+	UpdateRateSec int   // todo: explain
 }
 
 func NewCondition(cfg *Config) (*Condition, error) {
 	// todo
 	return nil, errors.New("not implemented")
+}
+
+func (c *Condition) UseBandwidth(size int) bool {
+	// todo
+	return false
+}
+
+func (c *Condition) CheckLoss() bool {
+	// todo
+	return false
+}
+
+func (c *Condition) AddLatency() int {
+	// todo
+	return 0
 }
 
 func (c *Condition) Close() {
